a8bc5e0b2ae08d6d7a6c7c7940cec0b2d410941b
anasiguer <anas.iguer@gmail.com>
2024-04-26 09:54:09 +0100

    first upload

---
+++ Boucle For.py
@@ -0,0 +1,12 @@
+'''
+Boucle For
+
+range           plage de valeurs
+range(5)        0 1 2 3 4 
+range(2,6)      2 3 4 5
+range(10,20,2)  10 12 14 16 18
+'''
+
+for i in range(5):
+    for j in range(2):
+        print("i",i,"j",j)
